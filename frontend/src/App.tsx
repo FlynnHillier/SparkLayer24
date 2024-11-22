6d06ce9ca27378b37f70bb49c4ee137a99d3b797
flynnhillier <flynnhillier@gmail.com>
2024-11-22 16:12:41 +0000

    quick refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,103 +1,69 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, {
+  Dispatch,
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
 import Todo, { TodoType } from "./Todo";
 import { SERVER_ADDRESS } from "./constants";
 
-function App() {
-  const [todos, setTodos] = useState<TodoType[]>([]);
-  const [addTodoRequestIsPending, setAddTodoRequestIsPending] = useState<boolean>(false);
+const AddTodoForm = forwardRef(
+  (
+    { setTodoList }: { setTodoList: Dispatch<React.SetStateAction<TodoType[]>> },
+    ref: ForwardedRef<HTMLFormElement>
+  ) => {
+    const [pendingTodo, setPendingTodo] = useState<TodoType>({ title: "", description: "" });
+    const [isAddTodoRequestPending, setIsAddTodoRequestPending] = useState<boolean>(false);
 
-  // I typically avoid using useState when working with objects - I would typically opt to use the 'useReducer' hook instead.
-  // However, due to the simplicity of the problem - in this scenario I think useState is ok.
-  const [pendingTodo, setPendingTodo] = useState<TodoType>({ title: "", description: "" });
+    //Initiate request to server to add ToDo
+    const requestAddToDo = useCallback(
+      async (todo: TodoType) => {
+        setIsAddTodoRequestPending(true);
 
-  const formRef = useRef<HTMLFormElement>(null);
+        try {
+          const res = await fetch(`${SERVER_ADDRESS}/`, {
+            method: "POST",
+            body: JSON.stringify(todo),
+          });
 
-  useEffect(() => {
-    formRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [todos]);
+          if (!res.ok) {
+            // Typically we'd add better error handling here - however doing so would likely be overengineering for this project?
+            throw Error(`Unexpected response status '${res.status}' when requesting to add TODO`);
+          }
 
-  // Initially fetch todo
-  // Typically I'd use react-query here - but seeing as you've already implemented this I will assume, in this scenario, a simple useEffect implemenation is ok.
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const todos = await fetch(`${SERVER_ADDRESS}/`);
-        if (todos.status !== 200) {
-          console.log("Error fetching data");
-          return;
+          const responseBody = await res.json();
+
+          setTodoList((prev) => [...prev, responseBody]);
+          clearPendingTodo();
+        } catch (e) {
+          console.log("Error requesting addition of TODO", e);
+        } finally {
+          setIsAddTodoRequestPending(false);
         }
+      },
+      [setTodoList, setIsAddTodoRequestPending]
+    );
 
-        setTodos(await todos.json());
-      } catch (e) {
-        console.log("Could not connect to server. Ensure it is running. " + e);
-      }
-    };
+    // Update the state of the pendingTodo
+    const updatePendingTodo = useCallback(
+      (toUpdate: Partial<TodoType>) => {
+        setPendingTodo((prev) => ({ ...prev, ...toUpdate }));
+      },
+      [setPendingTodo]
+    );
 
-    fetchTodos();
-  }, []);
+    // Clear the state of the pending todo (revert to empty)
+    const clearPendingTodo = useCallback(() => {
+      setPendingTodo({ title: "", description: "" });
+    }, [setPendingTodo]);
 
-  //Initiate request to server to add ToDo
-  const requestAddToDo = useCallback(
-    async (todo: TodoType) => {
-      setAddTodoRequestIsPending(true);
-
-      try {
-        const res = await fetch(`${SERVER_ADDRESS}/`, {
-          method: "POST",
-          body: JSON.stringify(todo),
-        });
-
-        if (!res.ok) {
-          // Typically we'd add better error handling here - however doing so would likely be overengineering for this project?
-          throw Error(`Unexpected response status '${res.status}' when requesting to add TODO`);
-        }
-
-        const responseBody = await res.json();
-
-        setTodos((prev) => [...prev, responseBody]);
-        clearPendingTodo();
-      } catch (e) {
-        console.log("Error requesting addition of TODO", e);
-      } finally {
-        setAddTodoRequestIsPending(false);
-      }
-    },
-    [setTodos, setAddTodoRequestIsPending]
-  );
-
-  // Update the state of the pendingTodo
-  const updatePendingTodo = useCallback(
-    (toUpdate: Partial<TodoType>) => {
-      setPendingTodo((prev) => ({ ...prev, ...toUpdate }));
-    },
-    [setPendingTodo]
-  );
-
-  // Clear the state of the pending todo (revert to empty)
-  const clearPendingTodo = useCallback(() => {
-    setPendingTodo({ title: "", description: "" });
-  }, [setPendingTodo]);
-
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>TODO</h1>
-      </header>
-
-      <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo
-            key={todo.title + todo.description}
-            title={todo.title}
-            description={todo.description}
-          />
-        ))}
-      </div>
-
-      <h2>Add a Todo</h2>
+    return (
       <form
-        ref={formRef}
+        ref={ref}
         onSubmit={(e) => {
           e.preventDefault();
 
@@ -126,13 +92,63 @@ function App() {
         <button
           type="submit"
           disabled={
-            addTodoRequestIsPending ||
+            isAddTodoRequestPending ||
             !(pendingTodo?.description.length && pendingTodo.title.length)
           }
         >
           Add Todo
         </button>
       </form>
+    );
+  }
+);
+
+function App() {
+  const [todoList, setTodoList] = useState<TodoType[]>([]);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    formRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [todoList]);
+
+  // Initially fetch todo
+  // Typically I'd use react-query here - but seeing as you've already implemented this I will assume, in this scenario, a simple useEffect implemenation is ok.
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const todos = await fetch(`${SERVER_ADDRESS}/`);
+        if (todos.status !== 200) {
+          console.log("Error fetching data");
+          return;
+        }
+
+        setTodoList(await todos.json());
+      } catch (e) {
+        console.log("Could not connect to server. Ensure it is running. " + e);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>TODO</h1>
+      </header>
+
+      <div className="todo-list">
+        {todoList.map((todo) => (
+          <Todo
+            key={todo.title + todo.description}
+            title={todo.title}
+            description={todo.description}
+          />
+        ))}
+      </div>
+
+      <h2>Add a Todo</h2>
+      <AddTodoForm setTodoList={setTodoList} ref={formRef} />
     </div>
   );
 }
